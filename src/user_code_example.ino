24b18c64341c91e26cc55f151a9c43025a6ccf63
tyeth <tyethgundry@googlemail.com>
2026-02-13 19:50:27 +0000

    Fix user code to turn on backlight and not redefine pins

--- src/user_code_example.ino
+++ src/user_code_example.ino
@@ -3,9 +3,19 @@
 #include <SPI.h>
 
 // Define TFT display pins
+/* incorrect - rely on built in pin definitions in pins_arduino.h
 #define TFT_CS   6  // Chip select pin
 #define TFT_DC  10  // Data/command pin
 #define TFT_RST  9  // Reset pin
+*/
+
+/* correct pins from pins_arduino.h - automatically used by platformio */
+// #define TFT_I2C_POWER 7
+// #define TFT_CS        42
+// #define TFT_RST       41
+// #define TFT_DC        40
+// #define TFT_BACKLITE  45
+
 
 // Initialize the TFT display
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
@@ -14,9 +24,13 @@ void setup() {
   // Initialize serial communication for debugging
   Serial.begin(115200);
   Serial.println("Initializing TFT...");
+  
+  //TODO: turn on backlite
+  pinMode(TFT_BACKLITE, OUTPUT);
+  digitalWrite(TFT_BACKLITE, HIGH);
 
   // Initialize the TFT display
-  tft.init(240, 135); // Initialize with width and height
+  tft.init(135, 240); // Initialize with width and height
   tft.setRotation(1); // Set display rotation
   tft.fillScreen(ST77XX_WHITE); // Clear the screen with black color
 
